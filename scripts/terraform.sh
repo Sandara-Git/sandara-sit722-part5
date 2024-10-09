48ca21e8db7ad251b1a5466095f3b6f9d8f32ed6
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-10-10 09:49:56 +1100

    Update terraform.sh

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -13,6 +13,12 @@ terraform validate
 echo "Import Existing Resource Group.."
 terraform import azurerm_resource_group.sit722sandara2 /subscriptions/973092b1-c341-496d-92fa-b8731afbd6e4/resourceGroups/sit722sandara2
 
+echo "Import Existing Container Registry"
+terraform import azurerm_container_registry.container_registry /subscriptions/973092b1-c341-496d-92fa-b8731afbd6e4/resourceGroups/sit722sandara2/providers/Microsoft.ContainerRegistry/registries/sit722sandara2cr
+
+echo "Import Existing Kubernetes Cluster"
+terraform import azurerm_kubernetes_cluster.cluster /subscriptions/973092b1-c341-496d-92fa-b8731afbd6e4/resourceGroups/sit722sandara2/providers/Microsoft.ContainerService/managedClusters/sit722sandara2
+
 echo "Creating Terraform plan..."
 terraform plan -out=tfplan
 
