7bd46b7cd42a99cb5f0cc5c40e99cfa04b972ccf
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-10-10 09:44:52 +1100

    Update terraform.sh

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -10,6 +10,9 @@ terraform init
 echo "Validating Terraform configuration..."
 terraform validate
 
+echo "Import Existing Resource Group.."
+terraform import azurerm_resource_group.sit722sandara2 /subscriptions/973092b1-c341-496d-92fa-b8731afbd6e4/resourceGroups/sit722sandara2
+
 echo "Creating Terraform plan..."
 terraform plan -out=tfplan
 
